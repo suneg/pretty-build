094e0cb8de93b67011823079cd475e20a273197e
Sune Gynthersen <sune.gynthersen@gmail.com>
2016-01-18 21:48:57 +0100

    forgot to add sharpziplib to build-mono.sh build script

--- build-mono.sh
+++ build-mono.sh
@@ -5,6 +5,7 @@ mcs -fullpaths /optimize- /debug+ /debug:full -platform:anycpu -target:exe \
     -out:Pretty.Build/bin/pretty.exe \
     -r:packages/NDesk.Options.0.2.1/lib/NDesk.Options.dll \
     -r:packages/log4net.2.0.3/lib/net40-full/log4net.dll \
+    -r:packages/SharpZipLib.0.86.0/lib/20/ICSharpCode.SharpZipLib.dll \
     Pretty.Build/BuildSpec.cs Pretty.Build/Pretty.cs Pretty.Build/PrettyParser.cs \
     Pretty.Build/ProjectType.cs Pretty.Build/Properties/AssemblyInfo.cs \
     Pretty.Build/Model/BuildResult.cs Pretty.Build/Model/Project.cs \
